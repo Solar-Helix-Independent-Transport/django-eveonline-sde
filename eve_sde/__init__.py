f42e48df82dbee425070299e0e27275a1a309bbb
Aaron Kable <aaronkable@gmail.com>
2026-08-16 13:04:58 +0800

    vbump

--- eve_sde/__init__.py
+++ eve_sde/__init__.py
@@ -1,5 +1,5 @@
 """Django EvE SDE Models"""
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 __title__ = "DjangoEvEOnlineSDE"
 __url__ = "https://github.com/Solar-Helix-Independent-Transport/django-eveonline-sde"
